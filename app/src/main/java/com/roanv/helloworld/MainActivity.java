9ebd59035c26fa6297f366aeaaf3d1252e64e2cb
Roan <roanvonb@gmail.com>
2017-05-23 10:31:53 +1000

    added "hello" button which displays "hello" message

--- app/src/main/java/com/roanv/helloworld/MainActivity.java
+++ app/src/main/java/com/roanv/helloworld/MainActivity.java
@@ -2,6 +2,9 @@ package com.roanv.helloworld;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,6 +12,16 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        final Button btn_hello = (Button) findViewById(R.id.btn_hello);
+        final TextView lbl_hello = (TextView) findViewById(R.id.lbl_hello);
+
+        btn_hello.setOnClickListener(new View.OnClickListener(){
+            public void onClick(View v){
+                lbl_hello.setVisibility(View.VISIBLE);
+            }
+        });
+
         //test comment
     }
 }
